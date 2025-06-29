8611bd9b3482c9602672bd7ddffc0b4282dc177c
Aditya Rizqi Ardhana <ardhanajames777@gmail.com>
2025-06-29 16:21:00 +0700

    delete unused code

--- src/pages/Dashboard/User/ListUser.jsx
+++ src/pages/Dashboard/User/ListUser.jsx
@@ -268,7 +268,6 @@ const ListUser = () => {
                                 </div>
                             </div>
                             <div className='flex items-center gap-2'>
-                                <input type="file" accept=".xlsx,.xls" onChange={handleChangeImportExcel} />
                                 <button onClick={open} className='bg-green-base rounded-[8px] border-0 py-2 px-4 text-white hover:bg-green-base/80 cursor-pointer flex items-center max-xs:w-full w-max montserrat gap-2'>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 7.5h-.75A2.25 2.25 0 0 0 4.5 9.75v7.5a2.25 2.25 0 0 0 2.25 2.25h7.5a2.25 2.25 0 0 0 2.25-2.25v-7.5a2.25 2.25 0 0 0-2.25-2.25h-.75m-6 3.75 3 3m0 0 3-3m-3 3V1.5m6 9h.75a2.25 2.25 0 0 1 2.25 2.25v7.5a2.25 2.25 0 0 1-2.25 2.25h-7.5a2.25 2.25 0 0 1-2.25-2.25v-.75" />
