6977634fc4fd53eb979fed4cfd31bbd4f345a3dd
Aditya Rizqi Ardhana <ardhanajames777@gmail.com>
2025-07-03 07:33:34 +0700

    fix home content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+// import { useState } from "react"
 
 import CommonLayout from "../layouts/CommonLayout"
 
@@ -8,14 +8,14 @@ import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "flowbite-rea
 
 const Home = () => {
 
-  const [openModal, setOpenModal] = useState(true);
+  // const [openModal, setOpenModal] = useState(true);
 
   return (
     <CommonLayout
       title='Beranda - Seleksi Tenaga Teknis Operasional Amdalnet 2025'
     >
       <div className="home-component mb-[5rem] max-2xs:mb-[4rem] max-3xs:mb-[2rem]">
-        <Modal show={openModal} onClose={() => setOpenModal(false)}>
+        {/* <Modal show={openModal} onClose={() => setOpenModal(false)}>
           <ModalHeader>Pengumuman, Harap Diperhatikan!</ModalHeader>
           <ModalBody>
             <div className="space-y-6">
@@ -28,7 +28,7 @@ const Home = () => {
           <ModalFooter>
             <Button onClick={() => setOpenModal(false)}>Mengerti</Button>
           </ModalFooter>
-        </Modal>
+        </Modal> */}
         <div className="jumbotron absolute top-[0] z-0">
             <img
                 width='auto'
@@ -43,11 +43,11 @@ const Home = () => {
           <h1 className="text-center text-gray-700 text-[2.9rem] max-xxxs:text-[5vw] max-3xs:text-[7vw] font-bold relative z-10">Kini Saatnya Menjadi Bagian dari <span className="text-green-base">AMDALNET</span> dan Berkontribusi untuk Indonesia!</h1>
           <h4 className="mt-4 text-neutral-500 font-medium text-[1.1rem] max-xxxs:text-[2vw] max-xs:text-[2.5vw] max-3xs:text-[12px] text-center leading-loose">PDLUK membuka lowongan untuk Putra Putri Terbaik Indonesia melalui Seleksi Tenaga Teknis Operasional Amdalnet 2025.</h4>
           <button onClick={() => {
-            window.open('https://www.s.id/hasil_administrasi', '_blank')
+            window.open('https://www.s.id/hasil_ujikompetensi', '_blank')
           }} className="bg-green-300/60 text-green-600 hover:bg-green-300 font-semibold rounded-[10px] border-0 py-3 max-2xs:py-2 px-8 max-2xs:px-5 cursor-pointer mx-auto max-xxxs:mt-8 mt-14 text-[1.1rem] max-xxxs:text-[2vw] max-2xs:text-[12px]">Lihat Pengumuman</button>
         </div>
         <div className="running-text marquee-container py-6 max-xxs:py-4 font-semibold bg-green-base mt-[6.5rem] max-xxs:mt-[4rem] max-3xs:mt-[2rem] relative z-10">
-            <h1 className="text-white max-xxs:text-[3vw] max-3xs:text-[4vw] text-[1.2rem] montserrat marquee-content"><strong>[INFORMASI]</strong> Pengumuman <strong>Hasil Seleksi Administrasi PENGADAAN TENAGA PENDUKUNG TEKNIS DOKUMEN LINGKUNGAN HIDUP AMDALNET</strong> telah disampaikan melalui laman <strong>amdalnet.menlhk.go.id</strong> pada portal "Berita Terkini".</h1>
+            <h1 className="text-white max-xxs:text-[3vw] max-3xs:text-[4vw] text-[1.2rem] montserrat marquee-content"><strong>[INFORMASI]</strong> Pengumuman <strong>Hasil Uji Kemampuan Teknis PENGADAAN TENAGA PENDUKUNG TEKNIS DOKUMEN LINGKUNGAN HIDUP AMDALNET</strong> telah disampaikan melalui laman <strong>amdalnet.menlhk.go.id</strong> pada portal "Berita Terkini".</h1>
         </div>
       </div>
     </CommonLayout>
