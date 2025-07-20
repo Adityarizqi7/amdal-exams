1352e907362a3202a2626b577cc0de5fc6786d04
Aditya Rizqi Ardhana <ardhanajames777@gmail.com>
2025-07-20 20:32:38 +0700

    fix word home section information

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,7 +47,7 @@ const Home = () => {
           }} className="bg-green-300/60 text-green-600 hover:bg-green-300 font-semibold rounded-[10px] border-0 py-3 max-2xs:py-2 px-8 max-2xs:px-5 cursor-pointer mx-auto max-xxxs:mt-8 mt-14 text-[1.1rem] max-xxxs:text-[2vw] max-2xs:text-[12px]">Lihat Pengumuman</button>
         </div>
         <div className="running-text marquee-container py-6 max-xxs:py-4 font-semibold bg-green-base mt-[6.5rem] max-xxs:mt-[4rem] max-3xs:mt-[2rem] relative z-10">
-            <h1 className="text-white max-xxs:text-[3vw] max-3xs:text-[4vw] text-[1.2rem] montserrat marquee-content"><strong>[INFORMASI]</strong> Pengumuman <strong>Hasil Uji Kemampuan Teknis PENGADAAN TENAGA PENDUKUNG TEKNIS DOKUMEN LINGKUNGAN HIDUP AMDALNET</strong> telah disampaikan melalui laman <strong>amdalnet.menlhk.go.id</strong> pada portal "Berita Terkini".</h1>
+            <h1 className="text-white max-xxs:text-[3vw] max-3xs:text-[4vw] text-[1.2rem] montserrat marquee-content"><strong>[INFORMASI]</strong> Pengumuman <strong> Akhir Seleksi PENGADAAN TENAGA PENDUKUNG TEKNIS DOKUMEN LINGKUNGAN HIDUP AMDALNET</strong> telah disampaikan melalui laman <strong>amdalnet.menlhk.go.id</strong> pada portal "Berita Terkini".</h1>
         </div>
       </div>
     </CommonLayout>
